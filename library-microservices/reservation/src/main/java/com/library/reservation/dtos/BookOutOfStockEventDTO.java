a57b53b6ca9b590496600bda6fbc2586fdeb3f16
Mathias-Veira <mveirad@gmail.com>
2026-04-06 21:02:04 +0200

    create DTO for kafka event in reservation service

---
+++ library-microservices/reservation/src/main/java/com/library/reservation/dtos/BookOutOfStockEventDTO.java
@@ -0,0 +1,25 @@
+package com.library.reservation.dtos;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+public class BookOutOfStockEventDTO {
+    private String eventId;
+    private int userId;
+    private int bookId;
+    private LocalDateTime timestamp;
+
+    public BookOutOfStockEventDTO(int userId, int bookId) {
+        this.eventId = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.bookId = bookId;
+        this.timestamp = LocalDateTime.now();
+    }
+}
