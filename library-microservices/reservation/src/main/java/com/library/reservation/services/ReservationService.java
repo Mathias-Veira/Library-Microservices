4ea9deca0223e5900a8b7de585b29009c9fdf790
Mathias-Veira <mveirad@gmail.com>
2026-04-06 21:02:37 +0200

    save reservation when book is out of stock

--- library-microservices/reservation/src/main/java/com/library/reservation/services/ReservationService.java
+++ library-microservices/reservation/src/main/java/com/library/reservation/services/ReservationService.java
@@ -1,4 +1,7 @@
 package com.library.reservation.services;
 
+import com.library.reservation.dtos.BookOutOfStockEventDTO;
+
 public interface ReservationService {
+    void saveReservation(BookOutOfStockEventDTO bookOutOfStockEventDTO);
 }
--- library-microservices/reservation/src/main/java/com/library/reservation/services/impl/ReservationServiceImpl.java
+++ library-microservices/reservation/src/main/java/com/library/reservation/services/impl/ReservationServiceImpl.java
@@ -1,8 +1,14 @@
 package com.library.reservation.services.impl;
 
+import com.library.reservation.dtos.BookOutOfStockEventDTO;
+import com.library.reservation.dtos.ReservationDTO;
+import com.library.reservation.mappers.ReservationMapper;
 import com.library.reservation.repositories.ReservationRepository;
 import com.library.reservation.services.ReservationService;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -10,5 +16,9 @@ public class ReservationServiceImpl implements ReservationService {
     public ReservationServiceImpl(ReservationRepository reservationRepository){
         this.reservationRepository = reservationRepository;
     }
-
+    @KafkaListener(topics = "book_out_of_stock", groupId = "library")
+    @Override
+    public void saveReservation(BookOutOfStockEventDTO bookOutOfStockEventDTO) {
+        reservationRepository.save(ReservationMapper.changeToEntity(new ReservationDTO(0, bookOutOfStockEventDTO.getBookId(), bookOutOfStockEventDTO.getUserId(), LocalDate.now())));
+    }
 }
