6a2b3a9c8e2aa67c202474fcc77885f77ddfeb4c
Mathias-Veira <mveirad@gmail.com>
2026-04-08 17:59:18 +0200

    create loan from kafka event

--- library-microservices/loan/src/main/java/com/library/loan/services/LoanService.java
+++ library-microservices/loan/src/main/java/com/library/loan/services/LoanService.java
@@ -1,7 +1,9 @@
 package com.library.loan.services;
 
+import com.library.loan.dtos.BookOutOfStockEventDTO;
 import com.library.loan.dtos.LoanBookDTO;
 import com.library.loan.dtos.LoanDTO;
+import org.springframework.kafka.annotation.KafkaListener;
 
 import java.util.List;
 
@@ -10,4 +12,5 @@ public interface LoanService {
     List<LoanBookDTO> getListLoansByUserId(int userId);
     LoanDTO saveLoan(int userId, int bookId);
     LoanDTO returnBook(int loanId);
+    void saveLoanFromReservationEvent(BookOutOfStockEventDTO bookOutOfStockEventDTO);
 }
--- library-microservices/loan/src/main/java/com/library/loan/services/impl/LoanServiceImpl.java
+++ library-microservices/loan/src/main/java/com/library/loan/services/impl/LoanServiceImpl.java
@@ -11,19 +11,22 @@ import com.library.loan.repositories.LoanRepository;
 import com.library.loan.services.BookClient;
 import com.library.loan.services.LoanService;
 import com.library.loan.services.UserClient;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
+
 @Service
 public class LoanServiceImpl implements LoanService {
     private final LoanRepository loanRepository;
     private final BookClient bookClient;
     private final UserClient userClient;
     private final KafkaTemplate<String, BookOutOfStockEventDTO> kafkaTemplate;
-    public LoanServiceImpl(LoanRepository loanRepository,BookClient bookClient, UserClient userClient,KafkaTemplate<String,BookOutOfStockEventDTO> kafkaTemplate){
+
+    public LoanServiceImpl(LoanRepository loanRepository, BookClient bookClient, UserClient userClient, KafkaTemplate<String, BookOutOfStockEventDTO> kafkaTemplate) {
         this.loanRepository = loanRepository;
         this.bookClient = bookClient;
         this.userClient = userClient;
@@ -36,13 +39,13 @@ public class LoanServiceImpl implements LoanService {
         //stores the loan and book data
         List<LoanBookDTO> loanBookDTOList = new ArrayList<>();
         //stores all the loans from a user
-        List<LoanDTO> loanDTOList =  LoanMapper.changeToListDTO(loanRepository.findLoansByUserId(userId));
+        List<LoanDTO> loanDTOList = LoanMapper.changeToListDTO(loanRepository.findLoansByUserId(userId));
         //stores books ids from the loans
         List<Integer> booksIds = loanDTOList.stream().map(LoanDTO::getBookId).distinct().collect(Collectors.toList());
         //stores book info
         List<BookDTO> bookDTOList = bookClient.getBooksById(booksIds);
         //store book info in a map
-        Map<Integer,BookDTO> bookDTOMap = bookDTOList.stream().collect(Collectors.toMap(BookDTO::getBookId,bookDTO -> bookDTO));
+        Map<Integer, BookDTO> bookDTOMap = bookDTOList.stream().collect(Collectors.toMap(BookDTO::getBookId, bookDTO -> bookDTO));
         //add books info into response object
         for (LoanDTO loanDTO : loanDTOList) {
             loanBookDTOList.add(new LoanBookDTO(loanDTO.getLoanId(), bookDTOMap.get(loanDTO.getBookId()), loanDTO.getUserId(), loanDTO.getLoanDate(), loanDTO.getLoanExpirationDate(), loanDTO.getLoanReturnDate()));
@@ -53,23 +56,29 @@ public class LoanServiceImpl implements LoanService {
     @Override
     public LoanDTO saveLoan(int userId, int bookId) {
         List<BookDTO> bookDTOList = bookClient.getBooksById(new ArrayList<>(List.of(bookId)));
-        if(userClient.findUserById(userId) == null || bookDTOList.isEmpty()){
+        if (userClient.findUserById(userId) == null || bookDTOList.isEmpty()) {
             throw new IdNotFoundException("The user or the book does not exist");
         }
         BookDTO bookDTO = bookDTOList.get(0);
-        if(bookDTO.getStock() <= 0){
-            kafkaTemplate.send("book_out_of_stock",new BookOutOfStockEventDTO(userId,bookId));
+        if (bookDTO.getStock() <= 0) {
+            kafkaTemplate.send("book_out_of_stock", new BookOutOfStockEventDTO(userId, bookId));
             return null;
         }
 
-        return LoanMapper.changeToDTO(loanRepository.save(new Loan(0,bookId,userId, LocalDate.now(),LocalDate.now().plusMonths(1),null)));
+        return LoanMapper.changeToDTO(loanRepository.save(new Loan(0, bookId, userId, LocalDate.now(), LocalDate.now().plusMonths(1), null)));
+    }
+
+    @KafkaListener(topics = "reservation_ready", groupId = "library")
+    @Override
+    public void saveLoanFromReservationEvent(BookOutOfStockEventDTO bookOutOfStockEventDTO) {
+        loanRepository.save(new Loan(0, bookOutOfStockEventDTO.getBookId(), bookOutOfStockEventDTO.getUserId(), LocalDate.now(), LocalDate.now().plusMonths(1), null));
     }
 
     @Override
     public LoanDTO returnBook(int loanId) {
         Loan loan = loanRepository.findById(loanId).orElseThrow(() -> new IdNotFoundException("The loan does not exist"));
         loan.setLoanReturnDate(LocalDate.now());
-        kafkaTemplate.send("book_returned", new BookOutOfStockEventDTO(loan.getUserId(),loan.getBookId()));
+        kafkaTemplate.send("book_returned", new BookOutOfStockEventDTO(loan.getUserId(), loan.getBookId()));
         return LoanMapper.changeToDTO(loanRepository.save(loan));
     }
 }
